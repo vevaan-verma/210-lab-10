3e38cf9400db0101a9de2fff35367b80040b7e9c
Vevaan <vevaan.s.verma@gmail.com>
2024-10-03 15:45:42 -0700

    Add hello world plus code block

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+// COMSC-210 | Lab 10 | Vevaan Verma
 using namespace std;
+#include <iostream>
 
 int main() {
 
-    // add any code here, sort of a 'hello world plus' code block
+	// add any code here, sort of a 'hello world plus' code block
+	cout << "Numbers in the range [1, 10]:" << endl;
 
-    return 0;
+	// loop through each number in the range [1, 10] and output it
+	for (int i = 1; i <= 10; i++)
+		cout << i << " ";
+
+	cout << endl; // output blank line at the end for formatting purposes
+
+	return 0;
+
 }
